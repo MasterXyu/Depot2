8ce6fdb8206248ae33a7ed4f4f3960a37fbf5f01
Administrator <nycsqllab@gmail.com>
2014-05-30 15:36:21 -0400

    Seed method changed+few images added

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,45 @@ Product.create(title:'Knee orthosis custom fitted',
                               The tird one crazy-expensive item that you typically get after car accident.</p>},
                image_url:'Knee_orthosis.png',
                price:800 )
+
+Product.create(title:'Wrist-hand-finger orthosis',
+               description: %{<p>
+                              Something for hand.</p>},
+               image_url:'android.png',
+               price:800 )
+
+Product.create(title:'Ankle orthosis custom fitted',
+               description: %{<p>
+                              Something custom.</p>},
+               image_url:'android.png',
+               price:800 )
+
+Product.create(title:'Walking Cane',
+               description: %{<p>
+                             Something for walking.</p>},
+               image_url:'android.png',
+               price:800 )
+
+Product.create(title:'Elbow orthosis custom fitted',
+               description: %{<p>
+                             Don't know what is it.</p>},
+               image_url:'android.png',
+               price:800 )
+
+Product.create(title:'Therapy cold/hot',
+               description: %{<p>
+                            The Therapy.</p>},
+               image_url:'android.png',
+               price:800 )
+
+Product.create(title:'Cervical traction equipment',
+               description: %{<p>
+                            Something.</p>},
+               image_url:'android.png',
+               price:800 )
+
+Product.create(title:'Cervical traction unit W/PUMP',
+               description: %{<p>
+                            Something with PUMP.</p>},
+               image_url:'android.png',
+               price:800 )
